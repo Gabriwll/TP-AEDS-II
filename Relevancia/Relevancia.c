e00113aacc18e3f12fda8bae50935b88be4e3730
Raíssa Tayná <raissa.xavier@ufv.br>
2025-06-28 21:15:22 -0300

    refactor relevancia.c

--- Relevancia/Relevancia.c
+++ Relevancia/Relevancia.c
@@ -1,88 +1,107 @@
+/**
+ * @file Relevancia.c
+ * @brief Implementa o cálculo da relevância de documentos com base no modelo TF-IDF.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include "Relevancia.h"
 
+/**
+ * @brief Limpa o buffer do teclado (stdin).
+ */
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {}
-} 
+}
 
-void iniciar_qtd_pala_arq(qtd_pala_arq *qtd){
-    int i;
-    for(i=0;i<TAM;i++){
+/**
+ * @brief Inicializa o vetor de quantidades de palavras por documento com zeros.
+ */
+void iniciar_qtd_pala_arq(qtd_pala_arq *qtd) {
+    for (int i = 0; i < TAM; i++) {
         qtd->v_qtd[i] = 0;
     }
 }
 
-void inserir_peso(relevancia *rel, int indice_arq, Arvore t, char *palavra, W *p_w){
-    double R;
-    int i;
-    for(i=0;i<TAM;i++){
-        p_w->v_W[i] += peso_W(t,palavra,i+1);
-        //printf("total W%d = %f\n\n",i+1,p_w->v_W[i]);
+/**
+ * @brief Insere o peso de uma palavra em todos os documentos.
+ */
+void inserir_peso(relevancia *rel, int indice_arq, Arvore t, char *palavra, W *p_w) {
+    for (int i = 0; i < TAM; i++) {
+        p_w->v_W[i] += peso_W(t, palavra, i + 1);
     }
-    return;
 }
 
-void inserir_qtd_pala_arq(qtd_pala_arq *qtd, int indice_arq, int qtd_pala){
+/**
+ * @brief Define o número de termos distintos no documento.
+ */
+void inserir_qtd_pala_arq(qtd_pala_arq *qtd, int indice_arq, int qtd_pala) {
     qtd->v_qtd[indice_arq] = qtd_pala;
 }
 
-int repeticao_pala_arq(Arvore t, char *palavra, int indice_arq){ //fij
-    Arvore i;
-    int j;
-    i = Pesquisa(palavra,&t);
-    if(i!=NULL){
-        for(j=0;j<M;j++){
-            if(i->V[j].arquivo == indice_arq){
-                //printf("repeticao_pala_arq = %d\n",i->V[j].repeticao);
+/**
+ * @brief Retorna o número de ocorrências de uma palavra em um documento (f_{j,i}).
+ */
+int repeticao_pala_arq(Arvore t, char *palavra, int indice_arq) {
+    Arvore i = Pesquisa(palavra, &t);
+    if (i != NULL) {
+        for (int j = 0; j < M; j++) {
+            if (i->V[j].arquivo == indice_arq) {
                 return i->V[j].repeticao;
             }
         }
     }
-    //printf("repeticao_pala_arq = 0\n");
     return 0;
 }
 
-int repeticao_pala_total(Arvore t, char *palavra){ //dj
-    Arvore i;
-    int cont=0,j;
-    i = Pesquisa(palavra,&t);
-    if(i!=NULL){
-        for(j=0;j<TAM;j++){
-            if(i->V[j].arquivo != 0)
+/**
+ * @brief Retorna o número de documentos que contêm a palavra (d_j).
+ */
+int repeticao_pala_total(Arvore t, char *palavra) {
+    Arvore i = Pesquisa(palavra, &t);
+    int cont = 0;
+    if (i != NULL) {
+        for (int j = 0; j < TAM; j++) {
+            if (i->V[j].arquivo != 0)
                 cont++;
         }
     }
-    //printf("quant_de_arq_tem_pala = %d\n",cont);
     return cont;
 }
 
-double peso_W(Arvore t, char *palavra, int indice_arq){
-    //printf("indice_arq = %d\n",indice_arq);
-    int f = repeticao_pala_arq(t,palavra,indice_arq), d = repeticao_pala_total(t,palavra);
+/**
+ * @brief Calcula o peso W do termo para o documento usando TF-IDF com log base 2.
+ * @return Peso W.
+ */
+double peso_W(Arvore t, char *palavra, int indice_arq) {
+    int f = repeticao_pala_arq(t, palavra, indice_arq);
+    int d = repeticao_pala_total(t, palavra);
     double W = 0;
-    if(f != 0 && d != 0)
-        W = f * (log10(TAM)/d);
-    //printf("W = %f\n",W);
+    if (f != 0 && d != 0)
+        W = f * (log2((double)TAM / d));  ///< log base 2 conforme enunciado
     return W;
 }
 
-void iniciar_W(W *w){
-    int i;
-    for(i=0;i<TAM;i++){
+/**
+ * @brief Inicializa o vetor de pesos com zero.
+ */
+void iniciar_W(W *w) {
+    for (int i = 0; i < TAM; i++) {
         w->v_W[i] = 0;
     }
 }
 
-void preencher_relevancia(relevancia *rel, W w, qtd_pala_arq qtd){
-    int i;
-    for(i=0;i<TAM;i++){
-        if(qtd.v_qtd[i] != 0)
-
-            rel->re[i] = (1.0/qtd.v_qtd[i]) * w.v_W[i];
+/**
+ * @brief Preenche o vetor de relevâncias de cada documento.
+ * @details Aplica a fórmula: r(i) = (1 / n_i) * soma(w_{j,i})
+ */
+void preencher_relevancia(relevancia *rel, W w, qtd_pala_arq qtd) {
+    for (int i = 0; i < TAM; i++) {
+        if (qtd.v_qtd[i] != 0)
+            rel->re[i] = (1.0 / qtd.v_qtd[i]) * w.v_W[i];
         else
             rel->re[i] = 0;
     }
